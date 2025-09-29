bec2a17f8316b0c3e771a32a41f6fa41ae65da0a
tensudink <tensudinku@gmail.com>
2025-09-29 02:09:44 -0400

    updated Api

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,6 +1,7 @@
 import axios  from "axios";
 const axiosInstance= axios.create({
-baseURL:"http://127.0.0.1:5001/clone-50cd3/us-central1/api"
+// baseURL:"http://127.0.0.1:5001/clone-50cd3/us-central1/api",
+baseURL: "https://api-waod5xptja-uc.a.run.app/",
 })
 
 
